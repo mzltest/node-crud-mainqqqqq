66dd67807534c0a585765b231c845bbf635c639f
mzltest <52124882+mzltest@users.noreply.github.com>
2022-11-02 00:49:23 +0800

    Update index.js

--- get/info/index.js
+++ get/info/index.js
@@ -4,7 +4,8 @@ let data = require('@begin/data')
 exports.handler = async function create(req) {
 //  let todo = arc.http.helpers.bodyParser(req).hashes
 let filekey=req.queryStringParameters.key
+if (filekey){
  res= await data.get({table:'indexes',key:filekey,limit:1})
  return res
-  
+  }
 }
