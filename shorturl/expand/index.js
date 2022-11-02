68795ccf6e39c1d3381f0c71637feed08d98cadc
mzltest <52124882+mzltest@users.noreply.github.com>
2022-11-02 17:36:47 +0800

    Update index.js

--- shorturl/expand/index.js
+++ shorturl/expand/index.js
@@ -3,7 +3,7 @@ let data = require('@begin/data')
 exports.handler = async function create(req) {
     let filekey=req.pathParameters.code
     if (filekey){
-res= await data.get({  table: 'surls',key:filekey})
+res= await data.get({  table: 'surls',key:filekey.toLowerCase()})
    return{
     statusCode: 302,
     headers: {
