08b3b1236ecbbfe59ddbce35abf95437bd45edcc
Ma Zl <yee@yee.bar>
2022-11-02 16:56:43 +0800

    ss

---
+++ shorturl/expand/index.js
@@ -0,0 +1,14 @@
+let data = require('@begin/data')
+
+exports.handler = async function create(req) {
+    let filekey=req.pathParameters.code
+    if (filekey){
+res= await data.get({  table: 'surls',key:filekey})
+   return{
+    statusCode: 302,
+    headers: {
+      'Location': '/?'+res.dest,
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    }
+   }
+  }}
---
+++ shorturl/shrink/index.js
@@ -0,0 +1,29 @@
+let data = require('@begin/data')
+function ranstr(length) {
+    var chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charLength = chars.length;
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+       result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+    return result;
+ }
+exports.handler = async function create(req) {
+    let filekey=req.pathParameters.key
+  pendingkey=ranstr(3)
+  while ((await data.get({  table: 'surls',key:pendingkey}))){
+    pendingkey=ranstr(3)
+  }
+
+  indexinfo = await data.set({
+    table: 'surls',
+    dest:filekey,
+    key:pendingkey,
+    ttl: (Date.now() / 1000) + (60 * 60 * 24 * 1)
+  })
+  return {
+    
+  html: "<script>document.write(window.document.domain+'/s/"+indexinfo.key+"')</script>"
+    
+  }
+}
