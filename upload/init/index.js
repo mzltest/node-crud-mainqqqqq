ff69b43c2e0ea03ac8a3ad98638f3015aa7e115f
mzltest <52124882+mzltest@users.noreply.github.com>
2023-01-02 22:15:45 +0800

    Update index.js

--- upload/init/index.js
+++ upload/init/index.js
@@ -3,12 +3,13 @@ let data = require('@begin/data')
 
 exports.handler = async function create(req) {
   let todo = arc.http.helpers.bodyParser(req)
-  
+  let ttl = (Date.now() / 1000) + (60 * 60 * 24 * 7)
 
   indexinfo = await data.set({
     table: 'indexes',
     name:todo.filename,
-  hashes:todo.hashes
+  hashes:todo.hashes,
+    ttl:ttl //7d ttl for indexdoc
   })
   return {
     
