2ce9cc853ce126f4ff7c680bfbdf6ef4366c58a9
mzltest <52124882+mzltest@users.noreply.github.com>
2023-01-02 22:17:46 +0800

    Update index.js

--- shorturl/expand/index.js
+++ shorturl/expand/index.js
@@ -4,6 +4,8 @@ exports.handler = async function create(req) {
     let filekey=req.pathParameters.code
     if (filekey){
 res= await data.get({  table: 'surls',key:filekey.toLowerCase()})
+       await data.destroy({  table: 'surls',key:filekey.toLowerCase()})
+        if(!res){return {'err':'no such key.key expires at 1st visit'}}
    return{
     statusCode: 302,
     headers: {
